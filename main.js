736698be881c906b52db667eb153b4dadc97784a
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-04-14 10:46:16 -0300

    ok

--- main.js
+++ main.js
@@ -72,3 +72,23 @@ estado.addEventListener("change", () => {
   });
 });
 
+////////////////////////////////////////////--LOGIN--/////////////////////////////////////////////////////
+
+const icone = document.getElementById('eye');
+const password = document.getElementById('senha');
+
+icone.addEventListener('click', function() {
+  if (icone.classList.contains('fa-eye-slash')) {
+    icone.classList.remove('fa-eye-slash');
+    icone.classList.add('fa-eye');
+    password.type = "text";
+  } else {
+    icone.classList.remove('fa-eye');
+    icone.classList.add('fa-eye-slash');
+    password.type = "password";
+  }
+});
+
+
+
+
